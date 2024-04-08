5da3b7680fa415d2e166e211646cf84d0b370065
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-04-08 16:06:04 +0900

    EZ-110 Feat: eazyPay register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecommendationPage from './pages/recommendation/RecommendationPage';
+import RegisterPage from './pages/register/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RecommendationPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/register/RegisterPage.jsx
@@ -0,0 +1,31 @@
+import DefaultLayout from '../../components/layout/DefaultLayout';
+import MainLogo from '../../assets/mainLogo.svg';
+import VerticalStepper from './VerticalStepper';
+import AgreementForm from './AgreementForm';
+import { useState } from 'react';
+
+const RegisterPage = () => {
+  const [stepperIndex, setStepperIndex] = useState(0);
+
+  return (
+    <DefaultLayout>
+      <div className="w-full h-full flex flex-col items-center justify-center bg-white p-10">
+        <img src={MainLogo} className="mb-1" />
+        <p className="text-base font-semibold text-slate-600 mb-10">
+          세상에서 가장 쉽고 편리한 결제
+        </p>
+
+        <div className="flex items-start w-full">
+          <VerticalStepper />
+          {stepperIndex === 0 && <AgreementForm />}
+        </div>
+
+        <button className="w-20 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 transition duration-300">
+          다음
+        </button>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default RegisterPage;
