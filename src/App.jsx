0e03d7dd9f018cbbbf175ad5194b56275903a405
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-11 18:24:29 +0900

    Feat: 로그인 리다이렉트 세션 스토리지

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecommendationPage from './pages/recommendation/RecommendationPage';
 import LoginPage from './pages/login/LoginPage';
+import { createContext, useState } from 'react';
+export const LoginUserContext = createContext();
 
 function App() {
+  const [loginUser, setLoginUser] = useState({});
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RecommendationPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RecommendationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </LoginUserContext.Provider>
   );
 }
 
