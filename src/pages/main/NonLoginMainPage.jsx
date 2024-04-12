e2400bba34d1aa06665ea5d9ae01a137d74e5fe1
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-12 16:01:38 +0900

    EZ-174 Chore: 누락파일 추가, 수정

--- src/pages/main/NonLoginMainPage.jsx
+++ src/pages/main/NonLoginMainPage.jsx
@@ -1,22 +1,8 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import DefaultLayout from '../../components/layout/DefaultLayout';
-import MainBenefits from './MainBenefits';
-import eazy from '../../assets/eAZyCard.svg';
 import CardCategorySearch from '../recommendation/CardCategorySearch';
-import CardPerfomance from '../main/CardPerformance';
-
-const userMain = {
-  userName: '박선주',
-  images: [
-    eazy,
-    'https://pc.wooricard.com/webcontent/cdPrdImgFileList/2023/7/17/e7b88886-8706-4bfd-be01-29ec5a77fd19.gif',
-    'https://pc.wooricard.com/webcontent/cdPrdImgFileList/2024/2/21/1fa0a89f-0811-43b1-9c24-a4bb8bf750f4.png',
-    'https://pc.wooricard.com/webcontent/cdPrdImgFileList/2023/7/17/bfee31ee-d644-4bc8-bbdb-1a4f78eb231c.gif',
-  ],
-  benefitAmount: 200000,
-};
-
+import OverlappedCard from '../../assets/overlappedCard.png';
 const omakase = {
   data: [
     {
@@ -146,34 +132,37 @@ const MainPage = () => {
   const navigate = useNavigate();
   const [checkedIndex, setcheckedIndex] = useState(0);
   const handleLegendClick = (seriesIndex) => {
-    // if (checkedIndex === seriesIndex) {
-    //   setcheckedIndex(-1);
-    // } else {
-    //   setcheckedIndex(seriesIndex);
-    // }
     setcheckedIndex(seriesIndex);
   };
 
-
-  useEffect(()=>{
-    if (sessionStorage.getItem('user') == null) {
-      navigate('/');
+  useEffect(() => {
+    if (sessionStorage.getItem('user') !== null) {
+      navigate('/main');
     }
-  },)
+  }, [navigate]);
+
   return (
     <>
       <DefaultLayout>
-        <div className="mt-[3rem] mb-8 text-2xl font-bold self-left">eAZy 하게 챙겼어요</div>
+        <div className="p-3 relative">
+          <div className="flex justify-center items-center flex-col absolute z-10 inset-0 bg-white bg-opacity-50 backdrop-filter backdrop-blur-sm">
+            <div className="mx-auto  mt-4 bottom-1/2 text-center text-3xl ">
+              로그인 하면 얼마나 혜택을 받을 수 있는지 알 수 있어요
+            </div>
+            <Link to="/login">
+              <div className="mt-[1rem] mx-auto w-[13rem] py-3 rounded-[20px] bg-[#1d92e9] text-2xl text-center text-white">
+                로그인 하기
+              </div>
+            </Link>
+          </div>
 
-        <div className="my-4">
-          <MainBenefits userMain={userMain} />
-        </div>
-
-        <div className="mt-8 text-2xl font-bold self-left">eAZy가 알아서 골라줬어요</div>
-        <div>
-          {userMain.images.map(
-            (image, index) => index !== 0 && <CardPerfomance key={index} image={image} />
-          )}
+          <img src={OverlappedCard} alt="Card" className="w-auto h-auto" />
+          <div className="absolute right-0 bottom-5">
+            <div className="mr-[15rem] flex-end relative">
+              <span className="text-5xl font-black text-blue-700 relative">???</span>
+              <span className="text-3xl">원</span>
+            </div>
+          </div>
         </div>
 
         <div className="my-3 w-auto h-[10em] bg-gray-500">Event</div>
