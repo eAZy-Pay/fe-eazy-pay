bb6fdd759a6bd2eab545863bfa1758f80b0182ef
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-16 16:39:48 +0900

    Chore: 메인배너 이름 표시되도록 수정

--- src/pages/main/MainBanner.jsx
+++ src/pages/main/MainBanner.jsx
@@ -8,17 +8,17 @@ import { getMainBanner } from '../../apis/CardAPI';
 
 const MainBanner = () => {
   const navigate = useNavigate();
+  const user = sessionStorage.getItem('user');
   const [userMain, setUserMain] = useState({
-    userName: '김이지',
+    userName: user ? JSON.parse(user).userName : 'OOO',
     images: [eazy],
     benefitAmount: 0,
     cards: [],
   });
-
   useEffect(() => {
-    if (sessionStorage.getItem('user')) {
-      getMainBanner(JSON.parse(sessionStorage.getItem('user')).uid).then((res) => {
-        console.log(res);
+    if (user) {
+      // 로그인 되어있는 경우
+      getMainBanner(JSON.parse(user).uid).then((res) => {
         const tmp = [eazy];
         res.cards.forEach((card) => {
           tmp.push(card.image);
@@ -26,7 +26,6 @@ const MainBanner = () => {
         setUserMain((prev) => ({
           ...prev,
           benefitAmount: res.benefitAmount,
-          userName: res.userName,
           images: tmp,
           cards: res.cards,
         }));
@@ -34,11 +33,12 @@ const MainBanner = () => {
     } else {
       navigate('/');
     }
-  }, [navigate]);
+  }, [navigate, user]);
+
   return (
     <>
       {/* 로그인 상태인 경우 */}
-      {sessionStorage.getItem('user') ? (
+      {user ? (
         userMain.cards.length ? (
           <>
             <div className="mt-[3rem] mb-8 text-2xl font-bold self-left">eAZy 하게 챙겼어요</div>
