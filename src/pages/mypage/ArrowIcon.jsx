6e684cad06bd52ef0254e982d16ef60c01db248b
[jimin326] <[jimincheon76@gmail.com]>
2024-04-17 17:39:56 +0900

    EZ-214 Feat: 마이페이지 컴포넌트 분리 &  세부 요소 제작

---
+++ src/pages/mypage/ArrowIcon.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const ArrowIcon = () => {
+  return <span className="material-symbols-outlined pt-[2px]">arrow_forward_ios</span>;
+};
+
+export default ArrowIcon;
---
+++ src/pages/mypage/AvailableFunds.jsx
@@ -0,0 +1,41 @@
+// import React from 'react';
+import DefaultFrame from '../../components/layout/DefaultFrame';
+import ArrowIcon from './ArrowIcon';
+import PropTypes from 'prop-types';
+
+const AmountDetail = ({ label, amount }) => (
+  <div className="flex justify-between font-bold">
+    <span className="text-[#666666]">{label}</span>
+    <span>{amount}원</span>
+  </div>
+);
+
+AmountDetail.propTypes = {
+  label: PropTypes.string,
+  amount: PropTypes.number,
+};
+
+const AvailableFunds = ({ funds, totalLimit, usedAmount }) => (
+  <DefaultFrame boxShadow={false}>
+    <div className="flex items-center justify-between px-6 py-4">
+      <h2 className="text-2xl font-extrabold">이용가능금액</h2>
+      <ArrowIcon />
+    </div>
+    <div className="px-6">
+      <span className="text-3xl font-extrabold">{funds}원</span>
+    </div>
+    <hr className="my-4 mx-6" />
+    <div className="flex flex-col p-6 text-xl gap-3">
+      <AmountDetail label="총 한도" amount={totalLimit} />
+      <AmountDetail label="이용한 금액" amount={usedAmount} />
+    </div>
+  </DefaultFrame>
+);
+
+AvailableFunds.propTypes = {
+  funds: PropTypes.number,
+  totalLimit: PropTypes.number,
+  usedAmount: PropTypes.number,
+};
+
+export default AvailableFunds;
