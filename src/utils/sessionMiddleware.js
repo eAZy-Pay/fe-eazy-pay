74f6bbe82fcb25bbc43903f2766a8a2775997391
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-19 16:02:46 +0900

    Chore: 세션 만료 시간 30분으로 변경

--- src/utils/sessionMiddleware.js
+++ src/utils/sessionMiddleware.js
@@ -11,8 +11,8 @@ export const sessionValidationCheck = () => {
     session = JSON.parse(decryptSession(session));
     const currentTime = Date.now();
     const timeElapsed = currentTime - session.timestamp; // 로그인으로부터 시간 차이 계산
-    // 3분(180000밀리초)이 지났는지 확인
-    if (timeElapsed > 8000) {
+    // 30분(1800000밀리초)이 지났는지 확인
+    if (timeElapsed > 1800000) {
       //테스트 위해 1.5분으로 수정
       // 시간이 지나면 sessionStorage에서 사용자 정보 삭제
       alert('세션이 만료되었습니다. 다시 로그인해주세요.');
