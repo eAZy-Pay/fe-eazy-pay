745fb2c599c9ccb6f966ac92b6a5775d3d315dae
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-04-22 11:40:25 +0900

    Refactor: 우리카드 회원 인증 후 데이터 불러오는 과정 삭제, Feat: 약관 동의에서 체크박스 기능 추가

--- src/pages/register/AgreementForm.jsx
+++ src/pages/register/AgreementForm.jsx
@@ -1,17 +1,60 @@
-const AgreementForm = () => {
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AgreementForm = ({ checkBox1, setCheckBox1, checkBox2, setCheckBox2 }) => {
+  const [allChecked, setAllChecked] = useState(false);
+
+  // 전체 동의 체크박스 클릭 시 호출되는 함수
+  const handleAllChecked = (event) => {
+    const isChecked = event.target.checked;
+    setAllChecked(isChecked);
+    setCheckBox1(isChecked); // 두 개의 필수 체크박스를 전체 동의 상태로 설정
+    setCheckBox2(isChecked);
+  };
+
+  // 각 필수 체크박스 클릭 시 호출되는 함수
+  const handleCheckBox1 = (event) => {
+    const isChecked = event.target.checked;
+    setCheckBox1(isChecked);
+    if (!isChecked) {
+      setAllChecked(false); // 하나라도 해제되면 "전체 동의" 체크박스를 해제
+    }
+  };
+  const handleCheckBox2 = (event) => {
+    const isChecked = event.target.checked;
+    setCheckBox2(isChecked);
+    if (!isChecked) {
+      setAllChecked(false); // 하나라도 해제되면 "전체 동의" 체크박스를 해제
+    }
+  };
+
   return (
     <div className="flex ml-36">
       <div className="flex flex-col">
         <div className="flex items-center mb-8">
-          <input type="checkbox" className="mr-4" />
-          <p className="w-80 text-left text-xl text-black">전체 동의</p>
+          <input
+            type="checkbox"
+            className="mr-4"
+            id="allCheck" // 체크박스의 고유한 ID를 지정
+            checked={allChecked}
+            onChange={handleAllChecked} // 전체 동의 상태 관리
+          />
+          <label htmlFor="allCheck" className="w-80 text-left text-xl text-black">
+            전체 동의
+          </label>
         </div>
 
         <div className="flex justify-between items-center mb-5 w-full max-w-md">
-          <input type="checkbox" className="mr-4" />
-          <p className="w-80 mr-4 text-left text-lg text-black">
+          <input
+            type="checkbox"
+            className="mr-4"
+            id="required1" // 각 필수 체크박스에 고유한 ID 지정
+            checked={checkBox1}
+            onChange={handleCheckBox1} // 첫 번째 필수 체크박스 상태 관리
+          />
+          <label htmlFor="required1" className="w-80 mr-4 text-left text-lg text-black">
             (필수) 개인정보 수집 및 이용·제공 동의
-          </p>
+          </label>
           <button className="py-1 px-4 bg-blue-100 text-[#808388] font-semibold rounded-lg hover:bg-blue-300 transition duration-300">
             보기
           </button>
@@ -26,10 +69,16 @@ const AgreementForm = () => {
         </div>
 
         <div className="flex justify-between items-center mb-5 w-full max-w-md">
-          <input type="checkbox" className="mr-4" />
-          <p className="w-80 mr-4 text-left text-lg text-black">
+          <input
+            type="checkbox"
+            className="mr-4"
+            id="required2"
+            checked={checkBox2}
+            onChange={handleCheckBox2} // 두 번째 필수 체크박스 상태 관리
+          />
+          <label htmlFor="required2" className="w-80 mr-4 text-left text-lg text-black">
             (필수) 개인정보 제3자 정보제공 동의
-          </p>
+          </label>
           <button className="py-1 px-4 bg-blue-100 text-[#808388] font-semibold rounded-lg hover:bg-blue-300 transition duration-300">
             보기
           </button>
@@ -44,6 +93,13 @@ const AgreementForm = () => {
       </div>
     </div>
   );
+};
+
+AgreementForm.propTypes = {
+  checkBox1: PropTypes.bool.isRequired,
+  setCheckBox1: PropTypes.func.isRequired,
+  checkBox2: PropTypes.bool.isRequired,
+  setCheckBox2: PropTypes.func.isRequired,
 };
 
 export default AgreementForm;
--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -6,17 +6,15 @@ import { useState } from 'react';
 import EazyPayRegisterForm from './EazyPayRegisterForm';
 import CardAuthenticationForm from './CardAuthenticationForm';
 import PinForm from './PinForm';
-import LoadData from './LoadData';
 import RegisterComplete from './RegisterComplete';
 import { submitAllData } from '../../apis/RegisterAPI';
 import { useNavigate } from 'react-router-dom';
 import { checkMember } from '../../apis/CheckMember';
-import NotLoadData from './NotLoadData';
 const RegisterPage = () => {
   const [stepperIndex, setStepperIndex] = useState(0);
-  const [loading, setLoading] = useState(false); // 로딩 중인 상태에만 true
-  const [dataLoaded, setDataLoaded] = useState(false); // 데이터 로드가 완료되면 true
-  const [memberValid, setMemberValid] = useState(false);
+  const [checkBox1, setCheckBox1] = useState(false);
+  const [checkBox2, setCheckBox2] = useState(false);
+  const [memberValid, setMemberValid] = useState(''); // 처음에 빈 문자열, 회원 인증이 완료되면 true 또는 false
   const [name, setName] = useState('');
   const [birthday, setBirth] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -26,18 +24,13 @@ const RegisterPage = () => {
   const [pin, setPin] = useState('');
   const navigate = useNavigate();
 
-  // 데이터 로딩이 완료되었을 때 loading 변수의 상태를 false로 변경
-  const handleDataLoaded = () => {
-    setLoading(false);
-    setDataLoaded(true);
-  };
-
   // 다음 버튼 클릭 로직
   const handleNext = () => {
-    console.log(`stepperIndex : ${stepperIndex}`);
+    console.log(`stepperIndex : ${stepperIndex}`); // 추후 로그 삭제
     if (stepperIndex === 1) {
       // 회원 인증 API 호출
       checkMember(name, phoneNumber, setMemberValid);
+      //
     }
     if (stepperIndex === 3) {
       regInfoSubmit();
@@ -62,6 +55,9 @@ const RegisterPage = () => {
 
   // 다음 버튼의 활성화/비활성화 조건 설정. true일 때 비활성화
   const isButtonDisabled = () => {
+    if (stepperIndex === 0) {
+      return !(checkBox1 && checkBox2);
+    }
     if (stepperIndex === 1) {
       return !(name && birthday && phoneNumber && email);
     }
@@ -86,7 +82,14 @@ const RegisterPage = () => {
         <div className="flex items-start w-full">
           <VerticalStepper activeStep={stepperIndex} setActiveStep={setStepperIndex} />
 
-          {stepperIndex === 0 && <AgreementForm />}
+          {stepperIndex === 0 && (
+            <AgreementForm
+              checkBox1={checkBox1}
+              setCheckBox1={setCheckBox1}
+              checkBox2={checkBox2}
+              setCheckBox2={setCheckBox2}
+            />
+          )}
           {stepperIndex === 1 && (
             <CardAuthenticationForm
               setName={setName}
@@ -95,32 +98,26 @@ const RegisterPage = () => {
               setEmail={setEmail}
             />
           )}
-          {/* 회원 인증 api 보내서 
-          200 ok 응답 받으면 상태를 true로 바꿔서 LoadData 보여주고
-          404 응답 받으면 상태를 false로 바꾸고 notLoadData 보여주기 */}
-          {stepperIndex === 2 &&
-            (memberValid ? <LoadData onLoadingComplete={handleDataLoaded} /> : <NotLoadData />)}
-          {stepperIndex === 3 && <EazyPayRegisterForm setId={setId} setPw={setPw} />}
-          {stepperIndex === 4 && <PinForm setValidPin={setPin} />}
-          {stepperIndex === 5 && <RegisterComplete memberValid={memberValid} />}
+
+          {stepperIndex === 2 && <EazyPayRegisterForm setId={setId} setPw={setPw} />}
+          {stepperIndex === 3 && <PinForm setValidPin={setPin} />}
+          {stepperIndex === 4 && <RegisterComplete memberValid={memberValid} />}
         </div>
 
-        {/*  로딩 중이 아닐 때만 버튼을 렌더링 */}
-        {!loading && (dataLoaded || stepperIndex < 6) && (
-          <button
-            className="w-20 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 transition duration-300"
-            onClick={handleNext}
-            disabled={isButtonDisabled()}
-          >
-            {stepperIndex === 4
-              ? '가입'
-              : stepperIndex === 5
-                ? memberValid
-                  ? '홈'
-                  : '카드 추천받기'
-                : '다음'}
-          </button>
-        )}
+        <button
+          className="w-20 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 transition duration-300"
+          onClick={handleNext}
+          disabled={isButtonDisabled()}
+        >
+          {stepperIndex === 4
+            ? '가입'
+            : stepperIndex === 5
+              ? memberValid
+                ? '홈'
+                : '카드 추천받기'
+              : '다음'}
+        </button>
+        {/* )} */}
       </div>
     </DefaultLayout>
   );
