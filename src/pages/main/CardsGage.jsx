b81bb177deb4cc8529f29c0c285471d237c233f4
[jimin326] <[jimincheon76@gmail.com]>
2024-04-24 13:35:17 +0900

    Fix: 백분율 100% 이상 애니메이션 오류 수정

--- src/pages/main/CardsGage.jsx
+++ src/pages/main/CardsGage.jsx
@@ -8,9 +8,14 @@ const CardsGage = ({ color, amount = 0, total = 1, width = 'w-full', margin = ' 
   useEffect(() => {
     // 백분율 계산
     const percentage = total > 0 ? (amount / total) * 100 : 0;
+    const finalWidth = percentage > 100 ? '100%' : `${percentage}%`; // 최종 width 설정
+
     // 마운트 후에 width 상태를 백분율 값으로 변경해 애니메이션 적용
-    const animationTimeout = setTimeout(() => setBarWidth(`${percentage}%`), 0);
-    return () => clearTimeout(animationTimeout);
+    const animationTimeout = setTimeout(() => setBarWidth(finalWidth), 0);
+    return () => {
+      setBarWidth('0%');
+      clearTimeout(animationTimeout);
+    };
   }, [amount, total]);
 
   return (
