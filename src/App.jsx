75f67eb08a5107d0a3f758a0938ae599329e36b0
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-04-24 14:56:58 +0900

    Feat: 인증 필요 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecommendationPage from './pages/recommendation/RecommendationPage';
 import LoginPage from './pages/login/LoginPage';
@@ -13,26 +14,95 @@ import QnaWritePage from './pages/qna/QnaWritePage';
 import CardDetailPage from './pages/card/CardDetailPage';
 import ManageMyInformation from './pages/mypage/ManageMyInformation';
 import MonthlyPayment from './pages/mypage/MonthlyPayment';
+import AdminCardPage from './pages/admin/AdminCardPage';
+import AuthRequiredPage from './pages/AuthRequiredPage';
+import CardApply from './pages/card/CardApply';
+
+export const ModalContext = createContext();
 
 function App() {
+  const [modal, setModal] = useState({
+    isOpen: false,
+    title: '',
+    content: '',
+  });
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<MainPage />} />
-        <Route path="/card-recommend" element={<RecommendationPage />} />
-        <Route path="/card-search" element={<CardSearchPage />} />
-        <Route path="/shopping" element={<ShoppingPage />} />
-        <Route path="/shopping/detail" element={<ShoppingDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/manage" element={<ManageMyInformation />} />
-        <Route path="/mypage/payment" element={<MonthlyPayment />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/qna" element={<QnaPage />} />
-        <Route path="/qna-write" element={<QnaWritePage />} />
-        <Route path="/card-detail/:id" element={<CardDetailPage />} />
-      </Routes>
+      <ModalContext.Provider value={{ modal, setModal }}>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/card-recommend" element={<RecommendationPage />} />
+          <Route path="/card-search" element={<CardSearchPage />} />
+          <Route path="/shopping" element={<ShoppingPage />} />
+          <Route path="/shopping/detail" element={<ShoppingDetail />} />
+          <Route
+            path="/mypage"
+            element={
+              <AuthRequiredPage>
+                <MyPage />
+              </AuthRequiredPage>
+            }
+          />
+          <Route
+            path="/mypage/manage"
+            element={
+              <AuthRequiredPage>
+                <ManageMyInformation />
+              </AuthRequiredPage>
+            }
+          />
+          <Route
+            path="/mypage/payment"
+            element={
+              <AuthRequiredPage>
+                <MonthlyPayment />
+              </AuthRequiredPage>
+            }
+          />
+
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+
+          <Route
+            path="/qna"
+            element={
+              <AuthRequiredPage>
+                <QnaPage />
+              </AuthRequiredPage>
+            }
+          />
+
+          <Route
+            path="/qna-write"
+            element={
+              <AuthRequiredPage>
+                <QnaWritePage />
+              </AuthRequiredPage>
+            }
+          />
+
+          <Route path="/card-detail/:id" element={<CardDetailPage />} />
+
+          <Route
+            path="/card-applicant/:id"
+            element={
+              <AuthRequiredPage>
+                <CardApply />
+              </AuthRequiredPage>
+            }
+          />
+
+          <Route
+            path="/admin/card"
+            element={
+              <AuthRequiredPage requiredAdmin={true}>
+                <AdminCardPage />
+              </AuthRequiredPage>
+            }
+          />
+        </Routes>
+      </ModalContext.Provider>
     </Router>
   );
 }
