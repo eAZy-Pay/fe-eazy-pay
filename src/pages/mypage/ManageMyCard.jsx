729672c3ff79e0cd9351f9a8400eb9edae7fb421
jimin <jimincheon76@gmail.com>
2024-04-29 03:07:44 +0900

    Chore: 일부 코드 수정

--- src/pages/mypage/ManageMyCard.jsx
+++ src/pages/mypage/ManageMyCard.jsx
@@ -85,14 +85,14 @@ const ManageMyCard = () => {
                   <img src={infoIcon} alt="Info Icon" />
                 </div>
                 <Link
-                  to="/mypage/manage/link-eazy"
+                  to="/mypage/card-management/link-eazy"
                   className="flex justify-center items-center w-60 h-12 rounded-xl border-2 border-gray-200 shadow-md hover:shadow-lg transition duration-300 ease-in-out"
                 >
                   <div className="text-xl text-center">eAZy 카드 연동 관리</div>
                 </Link>
               </div>
               {userCards.length >= 5 ? (
-                <Marquee className="w-[900px]" speed={30}>
+                <Marquee className="w-[900px] gap-8" speed={30}>
                   {linkedCards(userCards.slice(0, 4), gapStyle(userCards.length))}
                 </Marquee>
               ) : (
@@ -104,7 +104,7 @@ const ManageMyCard = () => {
 
         <div className="flex justify-between items-center text-2xl mt-16 mb-8">내 카드 관리</div>
         <div className="flex flex-wrap justify-between">
-          {userCards.map(({ card, num, useAmount, paymentLimit }, index) => {
+          {userCards.map(({ card, num, useAmount, paymentLimit, uid }, index) => {
             // num 문자열에서 마지막 4자리를 추출합니다.
             const lastFourDigits = num.slice(-4);
             // 마지막 4자리를 *로 대체합니다.
@@ -116,33 +116,35 @@ const ManageMyCard = () => {
                 className="mb-6"
                 style={{ width: '39.25rem' }}
               >
-                <DefaultFrame>
-                  <div className="flex flex-col p-4">
-                    <div className="flex">
-                      <RotatedCard
-                        image={card.image}
-                        style={{ maxWidth: '7rem', height: 'auto' }}
-                      />
-                      <div className="flex flex-col justify-center items-center gap-5 ml-11">
-                        <CardsGage
-                          color="#f79042"
-                          amount={useAmount}
-                          total={paymentLimit}
-                          width="w-3/4"
+                <Link to={`/mypage/card-management/selected-card`} state={uid}>
+                  <DefaultFrame>
+                    <div className="flex flex-col p-4">
+                      <div className="flex">
+                        <RotatedCard
+                          image={card.image}
+                          style={{ maxWidth: '7rem', height: 'auto' }}
                         />
-                        <CardLetter
-                          color="#f79042"
-                          label="총 혜택"
-                          amount={useAmount}
-                          total={paymentLimit}
-                        />
+                        <div className="flex flex-col justify-center items-center gap-5 ml-11">
+                          <CardsGage
+                            color="#f79042"
+                            amount={useAmount}
+                            total={paymentLimit}
+                            width="w-3/4"
+                          />
+                          <CardLetter
+                            color="#f79042"
+                            label="총 혜택"
+                            amount={useAmount}
+                            total={paymentLimit}
+                          />
+                        </div>
                       </div>
+                      <div className="text-2xl">{card.name}</div>
+                      {/* 마지막 4자리를 *로 대체한 값을 표시합니다. */}
+                      <div className="text-xl text-gray-400">카드 번호 {maskedNum}</div>
                     </div>
-                    <div className="text-2xl">{card.name}</div>
-                    {/* 마지막 4자리를 *로 대체한 값을 표시합니다. */}
-                    <div className="text-xl text-gray-400">카드 번호 {maskedNum}</div>
-                  </div>
-                </DefaultFrame>
+                  </DefaultFrame>
+                </Link>
               </div>
             );
           })}
@@ -150,12 +152,14 @@ const ManageMyCard = () => {
             style={{ width: '39.25rem', cursor: 'pointer' }} // 마지막에 카드 추가를 위한 공간 추가
             // onClick={() => navigate('/add-card')} // 카드 추가 페이지로 이동하는 이벤트
           >
-            <DefaultFrame className="flex justify-center h-[304.83px] ">
-              <div className="flex flex-col justify-center items-center gap-3 ">
-                <img src={plusIcon} alt="Plus Icon" />
-                <div className="text-2xl">새로운 카드를 추가해 보세요!</div>
-              </div>
-            </DefaultFrame>
+            <Link to="/card-search">
+              <DefaultFrame className="flex justify-center h-[304.83px] ">
+                <div className="flex flex-col justify-center items-center gap-3 ">
+                  <img src={plusIcon} alt="Plus Icon" />
+                  <div className="text-2xl">새로운 카드를 추가해 보세요!</div>
+                </div>
+              </DefaultFrame>
+            </Link>
           </div>
         </div>
       </DefaultLayout>
