e082af54e5af8d96d060134e8687d67bd69aa448
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-05-01 15:48:31 +0900

    Fix: error message

--- src/apis/UserAPI.js
+++ src/apis/UserAPI.js
@@ -57,7 +57,7 @@ export const checkUserCard = async (userId, cardId) => {
 
     // 사용자가 해당 카드를 가지고 있지 않은 경우 404 에러가 발생
     if (response.status === 404) {
-      return;
+      return null;
     }
 
     if (!response.ok) {
