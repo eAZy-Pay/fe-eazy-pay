62261dbd33d0d98f78a43514b162f7a1954845ba
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-05-02 01:44:56 +0900

    Chore: main page 디자인 수정

--- src/components/layout/DefaultFrame.jsx
+++ src/components/layout/DefaultFrame.jsx
@@ -4,11 +4,11 @@ const DefaultFrame = ({ children, className, boxShadow = true }) => {
   return (
     <>
       <div
-        className={`w-full rounded-2xl p-4 bg-white border-2 border-[#e2e2e2] ${className}`}
+        className={`w-full rounded-2xl p-4 bg-white border-[1px] border-[#e2e2e2] ${className}`}
         style={{
           backgroundColor: '#ffffff',
           boxShadow: boxShadow
-            ? '0 2px 4px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)'
+            ? '0 0.5px 1px rgba(0, 0, 0, 0.1), 0 0.5px 1px rgba(0, 0, 0, 0.06)'
             : 'none',
         }}
       >
