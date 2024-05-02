48488ccc0acb4c539e4768b6f09609a83484d84e
[jimin326] <[jimincheon76@gmail.com]>
2024-05-02 15:39:17 +0900

    Chore: 드롭다운메뉴 일부 수정

--- src/components/layout/DropDownMenu.jsx
+++ src/components/layout/DropDownMenu.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import ProfileIcon from '../../assets/profileIcon.svg';
+import ProfileImage from '../../assets/profileImage.png';
 import CardIcon from '../../assets/cardIcon.svg';
 import LogoutIcon from '../../assets/logoutIcon.svg';
 import HelpIcon from '../../assets/helpIcon.svg';
@@ -14,23 +14,26 @@ const DropdownMenu = () => {
   return (
     <>
       <div
-        className={`flex flex-col  items-center w-[200px] h-auto absolute right-0 mt-2 bg-white rounded-md shadow-xl z-20 ${!isDropdownOpen && 'hidden'}`}
+        className={`flex flex-col justify-center items-center w-[200px] h-auto absolute right-0 mt-2 bg-white rounded-md shadow-xl py-2 z-20 ${!isDropdownOpen && 'hidden'}`}
         style={{ top: '100%', border: '1px solid #E5E7EB' }} // 부모 컴포넌트의 높이만큼 아래에 위치
       >
         {user ? (
           <>
             <Link to="/mypage/manage">
-              <div className="cursor-pointer flex justify-center py-6 gap-4">
-                <img src={ProfileIcon} alt="Profile Icon" />
-                <div className="flex items-center text-xl text-left">{user.userName} 님</div>
+              <div className="cursor-pointer flex justify-center p-3 gap-1">
+                <img src={ProfileImage} alt="Profile Icon" className='w-[3em]' />
+                <div className='flex items-center text-xl text-left gap-1'>
+                  <div className='text-[#4A3AFF]'>{user.userName}</div>
+                  <div>님</div>
+                </div>
               </div>
             </Link>
             <hr className="bg-gray-300 w-[85%] mb-3 " />
           </>
         ) : (
           <>
-            <div className="cursor-pointer flex justify-center py-6 gap-4">
-              <img src={ProfileIcon} alt="Profile Icon" />
+            <div className="cursor-pointer flex justify-center p-3 gap-1">
+              <img src={ProfileImage} alt="Profile Icon" className='w-[3em]' />
               <div
                 className="flex items-center text-lg text-left"
                 onClick={() => navigate('/login')}
@@ -42,13 +45,13 @@ const DropdownMenu = () => {
           </>
         )}
         <div className="cursor-pointer flex justify-center p-3 gap-4">
-          <img src={CardIcon} alt="Card Icon" className="w-[2em]" />
+          <img src={CardIcon} alt="Card Icon" className="w-[1.8em]" />
           <Link to="/mypage/card-management" className="flex items-center text-xl text-left">
             카드관리
           </Link>
         </div>
         <div className="cursor-pointer flex justify-center p-3 gap-4">
-          <img src={HelpIcon} alt="Help Icon" className="w-[2em]" />
+          <img src={HelpIcon} alt="Help Icon" className="w-[1.8em]" />
           <Link to="/faq" className="flex items-center text-xl text-left">
             문의하기
           </Link>
@@ -63,7 +66,7 @@ const DropdownMenu = () => {
               }}
             >
               {/*서버 세션 발급 확장성을 위해 Link 태그로 나뒀습니다.*/}
-              <img className="w-[2em]" src={LogoutIcon} alt="Logout Icon" />
+              <img className="w-[1.8em]" src={LogoutIcon} alt="Logout Icon" />
               <Link className="flex items-center text-xl text-left">로그아웃</Link>
             </div>
           </>
