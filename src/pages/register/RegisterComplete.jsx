eb404b8d794df9ed439fa8924bc96dc8152174e7
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-05-05 23:38:39 +0900

    Chore: 회원 가입 완료 페이지 아이콘 추가

--- src/pages/register/RegisterComplete.jsx
+++ src/pages/register/RegisterComplete.jsx
@@ -1,10 +1,11 @@
+import checkCircle from '../../assets/checkCircleIcon.svg';
 const RegisterComplete = () => {
   return (
     <>
       <div className="bg-white p-10 w-full shadow-lg rounded-lg flex flex-col items-center">
-        <h1 className="text-lg font-bold text-center">이지페이 가입이 완료됐어요!</h1>
-        <h1 className="text-lg font-bold text-center">이지카드를 사용할 수 있어요.</h1>
-        <h1 className="text-lg font-bold text-center">이지카드로 혜택과 실적을 관리하세요.</h1>
+        <img src={checkCircle} className="w-20 h-20 opacity-25 mb-10" />
+        <p className="text-2xl font-bold text-center">이지페이 회원 가입 완료</p>
+        <p className="text-lg font-bold text-center">이지카드로 혜택과 실적을 모두 관리하세요.</p>
       </div>
     </>
   );
