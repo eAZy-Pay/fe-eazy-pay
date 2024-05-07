bf588553b7b690f2900dfd7c2d80fab11f6135c7
PARK SEONJU <sj.ella1030@gmail.com>
2024-05-07 17:53:02 +0900

    Feat: 404 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import CardApplyPage from './pages/card/CardApplyPage';
 import ManageLinkEazy from './pages/mypage/ManageLinkEazy';
 import SelectedCard from './pages/mypage/SelectedCard';
 import AdminQnaPage from './pages/admin/AdminQnaPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export const ModalContext = createContext();
 export const DropdownContext = createContext();
@@ -48,6 +49,7 @@ function App() {
         <DropdownContext.Provider value={{ isDropdownOpen, setIsDropdownOpen }}>
           <CurrentPageContext.Provider value={{ currentPage, setCurrentPage }}>
             <Routes>
+              <Route path="/*" element={<NotFoundPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/" element={<MainPage />} />
               <Route path="/card-recommend" element={<RecommendationPage />} />
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import NotFoundImage from '../assets/NotFoundImage.svg';
+const NotFoundPage = () => {
+  return (
+    <div className='h-full w-full flex justify-center items-center'>
+      <a href='/' className="flex flex-col text-xl w-[25rem] mt-[4rem]">
+        <img src={NotFoundImage} alt="404" />
+        <div className='mx-auto'>요청하신 페이지를 찾을 수 없어요</div>
+      </a>
+    </div>
+  );
+};
+
+export default NotFoundPage;
